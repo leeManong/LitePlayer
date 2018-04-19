8a391d7d0745bff8e7c45d7b64ba81710445486c
lmj <767321403@qq.com>
2018-04-19 22:24:05 +0800

    First commit

--- app/src/main/java/com/limingjian/liteplayer/MainActivity.java
+++ app/src/main/java/com/limingjian/liteplayer/MainActivity.java
@@ -1,26 +1,39 @@
 package com.limingjian.liteplayer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 
+import com.blankj.utilcode.util.FragmentUtils;
+import com.limingjian.liteplayer.business.filemanager.FileManagerFragment;
+import com.limingjian.liteplayer.business.videodirectory.VideoDirectoryFragment;
+import com.limingjian.liteplayer.callback.OnBackPressedCallBack;
+import com.limingjian.liteplayer.callback.OnSetToolBarTitleCallBack;
+import com.limingjian.liteplayer.utils.ActivityUtils;
+import com.limingjian.liteplayer.utils.GetFilesUtils;
 import com.limingjian.liteplayer.widget.AutoClearEditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.jzvd.JZVideoPlayerStandard;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnSetToolBarTitleCallBack {
 
     @BindView(R.id.main_edit_search)
     AutoClearEditText mMainEditSearch;
@@ -33,11 +46,18 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.dl_main_activity)
     DrawerLayout mDlMainActivity;
 
+    private VideoDirectoryFragment mVideoDirectoryFragment;
+
+    private FileManagerFragment mFileManagerFragment;
+
+    private static OnBackPressedCallBack mOnBackPressedCallBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
 
         initView();
 
@@ -47,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
+
     }
 
     private void initView() {
@@ -63,6 +84,75 @@ public class MainActivity extends AppCompatActivity {
                 openDrawer(mDlMainActivity);
             }
         });
+        mNavMain.setCheckedItem(R.id.nav_video);
+        mNavMain.setNavigationItemSelectedListener(getNavigationItemSelectedListener());
+
+        if (mVideoDirectoryFragment == null) {
+            mVideoDirectoryFragment = new VideoDirectoryFragment();
+        }
+        FragmentUtils.add(getSupportFragmentManager(), mVideoDirectoryFragment, R.id.fl_main_container);
+
+    }
+
+    private NavigationView.OnNavigationItemSelectedListener getNavigationItemSelectedListener() {
+        return new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_video:
+                        goVideo();
+                        return true;
+                    case R.id.nav_directory:
+                        return goDirectory();
+                }
+                return false;
+            }
+        };
+    }
+
+    private boolean goDirectory() {
+        closeDrawer(mDlMainActivity);
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+        if (mFileManagerFragment == null) {
+            mFileManagerFragment = FileManagerFragment.newInstance(GetFilesUtils.getInstance().getBasePath(), "内部存储");
+            for (int i = 0; i < count; i++) {
+                getSupportFragmentManager().popBackStack();
+            }
+        }
+
+        //FragmentUtils.add(getSupportFragmentManager(), mFileManagerFragment, R.id.fl_main_container, false);
+        ActivityUtils.addFragmentToActivity(getSupportFragmentManager(),mFileManagerFragment,R.id.fl_main_container,false);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+        }
+    }
+
+    private void goVideo() {
+        closeDrawer(mDlMainActivity);
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+        //清空回退栈
+        for (int i = 0; i < count; i++) {
+            getSupportFragmentManager().popBackStack();
+        }
+
+        /*if (mVideoDirectoryFragment != null && mVideoDirectoryFragment.isAdded()) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.remove(mVideoDirectoryFragment);
+            transaction.commit();
+            mVideoDirectoryFragment = null;
+        }*/
+
+        if (mFileManagerFragment != null && mFileManagerFragment.isAdded()) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.remove(mFileManagerFragment);
+            transaction.commit();
+            mFileManagerFragment = null;
+        }
     }
 
     @Override
@@ -74,7 +164,28 @@ public class MainActivity extends AppCompatActivity {
     private void openDrawer(DrawerLayout drawerLayout) {
         drawerLayout.openDrawer(Gravity.START);
     }
+
     private void closeDrawer(DrawerLayout drawerLayout) {
         drawerLayout.closeDrawer(Gravity.START);
+    }
+
+    @Override
+    public void setToolBarTitle(String title) {
+        mTbMain.setTitle(title);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mOnBackPressedCallBack != null) {
+            mOnBackPressedCallBack.activityOnBackPressed();
+        }
+        if (JZVideoPlayerStandard.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    public static void setOnBackPressedCallBack(OnBackPressedCallBack onBackPressedCallBack) {
+        mOnBackPressedCallBack = onBackPressedCallBack;
     }
 }
